81f0158f5b7832cb8a43c615ef10ce1d030926de
wanderson-rpf <wandersonrpf@gmail.com>
2023-05-26 11:51:23 -0300

    :construction: [progress] Aula 5: Dados no template.

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.scss']
 })
 export class FirstComponentComponent {
-
+  public name: string = 'Wanderson';
+  public age: number = 33;
+  public job: string = 'Dev';
+  public hobbies: Array<String> = ['Correr', 'Jogar', 'Estudar']
+  public car: { name: string, year: number} = {
+    name: 'Polo',
+    year: 2020,
+  }
 }
